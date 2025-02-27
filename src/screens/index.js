e68eaefca5186959de7da43c6e1471e96153ad91
Pardoardo <eduardo.escolar447@gmail.com>
2025-02-26 22:10:26 -0300

    Tentando processar os valores

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,54 @@
 import { useState, React } from "react";
-import { Text, Picker, TouchableOpacity, TextInput, View, SafeAreaView, StyleSheet } from "react-native";
+import { Text, Picker, TouchableOpacity, TextInput, View, SafeAreaView, StyleSheet, Alert } from "react-native";
 
 export default function Home() {
+
+    const [ idade, setIdade ] = useState();
+    const [ peso, setPeso ] = useState();
+    const [ altura, setAltura ] = useState();
+    const [ sexo, setSexo ] = useState();
+
+    const setarValorSexo = (evento) => {
+        setSexo(evento.target.value)
+        console.log("novo sexo definido: " + evento.target.value)
+    }
+    
+    function Calcular() {
+        const alt = parseInt(altura)
+        console.log(alt)
+        function verificarDados(){
+            if(!isNaN(parseInt(altura))){
+                console.log("altura válida")
+            } else {
+                Alert.alert(
+                    'Informações Inválidas',
+                    'A altura digitada não é válida!',
+                    [{ text: 'Ok' }],
+                    { cancelable: true }
+                )
+                alert('A altura digitada não é válida!')
+            }
+        }
+
+        function verificarPreenchimento() {
+            if (!idade == '' && !peso == '' && !altura == '') {
+                verificarDados()
+            } else {
+                Alert.alert(
+                    'Informações faltando',
+                    'Você não prencheeu todas as lacunas necessárias!',
+                    [{ text: 'Ok' }],
+                    { cancelable: true }
+                )
+                alert('Você não prencheeu todas as lacunas necessárias!')
+                console.log(altura, idade, peso, sexo)
+                return;
+            }
+        }
+
+        verificarPreenchimento()
+
+    }
 
     return (
         <SafeAreaView style={esquilos.container}>
@@ -9,21 +56,20 @@ export default function Home() {
                 <Text style={esquilos.titulo}>Calculadora de Gasto Calórico Diário</Text>
 
                 <View style={esquilos.dados}>
-                    <TextInput style={esquilos.caixa_informacao} placeholder="Insira Idade" />
-                    <TextInput style={esquilos.caixa_informacao} placeholder="Insira Peso" />
-                    <TextInput style={esquilos.caixa_informacao} placeholder="Insitra Altura" />
-                    <Picker style={esquilos.caixa_informacao}>
+                    <TextInput style={esquilos.caixa_informacao} placeholder="Insira Idade" onChangeText={setIdade} />
+                    <TextInput style={esquilos.caixa_informacao} placeholder="Insira Peso" onChangeText={setPeso} />
+                    <TextInput style={esquilos.caixa_informacao} placeholder="Insitra Altura" onChangeText={setAltura} />
+                    <Picker style={esquilos.caixa_informacao} onChange={setarValorSexo}>
                         <Picker.Item label="Selecione sexo" value={undefined} />
                         <Picker.Item label="Masculino" value='m' />
                         <Picker.Item label="Feminino" value='f' />
                     </Picker>
                 </View>
 
-                <TouchableOpacity style={esquilos.botao}>
+                <TouchableOpacity style={esquilos.botao} onPress={Calcular}>
                     <Text style={esquilos.texto_botao}>Calcular</Text>
                 </TouchableOpacity>
             </View>
-
         </SafeAreaView>
     )
 }
